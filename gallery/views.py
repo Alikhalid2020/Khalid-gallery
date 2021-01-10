51d59abf29e21dfcf477edc3e1c205a4b1bf0644
Alikhalid2020 <lanrakhaled@gmail.com>
2021-01-10 10:39:41 +0300

    added past and todays gallery to views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,5 @@
 from django.http  import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
@@ -8,3 +7,22 @@ def welcome(request):
 def gallery_of_day(request):
     date = dt.date.today()
     return render(request, 'all-gallery/today-gallery.html', {"date": date,})
+
+
+#......
+# View Function to present gallery from past days
+def past_days_gallery(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(gallery_of_day)
+
+    return render(request, 'all-gallery/past-gallery.html', {"date": date})
