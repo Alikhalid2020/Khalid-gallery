06285284411ed3076e9a5673322f17e1df8ba6ed
Alikhalid2020 <lanrakhaled@gmail.com>
2021-01-11 23:45:20 +0300

    added url file

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
+    # url('^$',views.today_imae,name = 'welcome'),
     url(r'$',views.gallery_today,name='galleryToday'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
